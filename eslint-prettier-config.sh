680ad422bc4e33993c264b55a63114ffde623a4c
giovankes <hello@giotje.dev>
2022-09-28 11:15:24 +0200

    added --legacy-peer-deps to npm install to support older projects

--- eslint-prettier-config.sh
+++ eslint-prettier-config.sh
@@ -19,7 +19,7 @@ echo "Which package manager are you using?"
 select package_command_choices in "Yarn" "npm" "Cancel"; do
   case $package_command_choices in
     Yarn ) pkg_cmd='yarn add'; break;;
-    npm ) pkg_cmd='npm install'; break;;
+    npm ) pkg_cmd='npm install --legacy-peer-deps'; break;;
     Cancel ) exit;;
   esac
 done
